c9cd6937b741aa53dc9169d43d11181c4352ca12
Kim <7533524@qq.com>
2019-12-08 19:35:43 +0800

    edfgdg

--- express/base/app.ts
+++ express/base/app.ts
@@ -1,78 +1,67 @@
 import axios from '../../src/index'
 
-// axios({
-//   method: 'get',
-//   url: '/base/get',
-//   params: {
-//     foo: ['bar', 'baz']
-//   }
-// })
-//
-// axios({
-//   method: 'get',
-//   url: '/base/get',
-//   params: {
-//     foo: {
-//       bar: 'baz'
-//     }
-//   }
-// })
-//
-// const date = new Date()
-//
-// axios({
-//   method: 'get',
-//   url: '/base/get',
-//   params: {
-//     date
-//   }
-// })
-//
-// axios({
-//   method: 'get',
-//   url: '/base/get',
-//   params: {
-//     foo: '@:$, '
-//   }
-// })
-//
-// axios({
-//   method: 'get',
-//   url: '/base/get',
-//   params: {
-//     foo: 'bar',
-//     baz: null
-//   }
-// })
-//
-// axios({
-//   method: 'get',
-//   url: '/base/get#hash',
-//   params: {
-//     foo: 'bar'
-//   }
-// })
-//
-// axios({
-//   method: 'get',
-//   url: '/base/get?foo=bar',
-//   params: {
-//     bar: 'baz'
-//   }
-// })
-//
-// axios({
+axios('/base/get', {
+  method: 'get',
+  params: {
+    foo: ['bar', 'baz']
+  }
+})
+
+axios('/base/get', {
+  method: 'get',
+  params: {
+    foo: {
+      bar: 'baz'
+    }
+  }
+})
+
+axios('/base/get', {
+  method: 'get',
+  params: {
+    date: new Date()
+  }
+})
+
+axios('/base/get',{
+  method: 'get',
+  params: {
+    foo: '@:$, '
+  }
+})
+
+axios('/base/get',{
+  method: 'get',
+  params: {
+    foo: 'bar',
+    baz: null
+  }
+})
+
+axios('/base/get',{
+  method: 'get',
+  params: {
+    foo: 'bar'
+  }
+})
+
+axios('/base/get',{
+  method: 'get',
+  params: {
+    bar: 'baz'
+  }
+})
+
+// axios('/base/get',{
 //   method: 'post',
-//   url: '/base/post',
 //   data: {
 //     a: 1,
 //     b: 2
 //   }
 // })
-//
-// axios({
+
+// axios('/base/get',{
 //   method: 'post',
-//   url: '/base/post',
 //   headers: {
 //     'content-type': 'application/json;charset=utf-8'
 //   },
@@ -81,6 +70,8 @@ import axios from '../../src/index'
 //     b: 2
 //   }
 // })
+
+
 //
 // const arr = new Int32Array([21, 31])
 //
@@ -100,25 +91,25 @@ import axios from '../../src/index'
 //   data: searchParams
 // })
 
-axios({
-  method: 'post',
-  url: '/base/post',
-  data: {
-    a: 1,
-    b: 2
-  }
-}).then((res) => {
-  console.log(res)
-})
+// axios({
+//   method: 'post',
+//   url: '/base/post',
+//   data: {
+//     a: 1,
+//     b: 2
+//   }
+// }).then((res) => {
+//   console.log(res)
+// })
 
-axios({
-  method: 'post',
-  url: '/base/post',
-  responseType: 'json',
-  data: {
-    a: 3,
-    b: 4
-  }
-}).then((res) => {
-  console.log(res)
-})
+// axios({
+//   method: 'post',
+//   url: '/base/post',
+//   responseType: 'json',
+//   data: {
+//     a: 3,
+//     b: 4
+//   }
+// }).then((res) => {
+//   console.log(res)
+// })
--- express/server.js
+++ express/server.js
@@ -7,7 +7,6 @@ const WebpackConfig = require('./webpack.config')
 
 //-----------------------------------------  创建express实例   -----------------------------------------
 const app = express()
-const Router = express.Router()
 const url = require('url');
 //-----------------------------------------------------------------------------------------------------
 const compiler = webpack(WebpackConfig)
@@ -23,25 +22,14 @@ app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 //--------------------------------       引进服务端路由     ------------------------------------------------
-Router.get('/page/get', function (req, res) {
-  const pathname = url.parse(req.url, true).pathname;
-  console.log(req, req)
-  // res.writeHead(200, {'Content-Type': 'text/plain'}); //下载的
-  // res.setHeader("Content-Type", "text/html");
-  // res.setHeader("Set-Cookie", ["type=ninja", "language=javascript"]);
-  // res.header("Access-Control-Allow-Origin", "*");
-  // res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
-  // res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-  // res.header("Cache-Control", "no-store");//304
-  // next();
-  res.json({ code: 0, success: true, relust: [{ pathname: pathname }], msg: `你请求的参数成功` })
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write(`你的请求路径是${pathname}`);
-  res.end();
-})
-
-app.use(Router)
-
+const router = express.Router()
+registerSimpleRouter()
+registerBaseRouter()
+registerErrorRouter()
+registerExtendRouter()
+registerInterceptorRouter()
+registerConfigRouter()
+app.use(router)
 //---------------------------------------------------------------------------------------------------------
 const port = process.env.PORT || 8080
 module.exports = app.listen(port, (err) => {
@@ -50,3 +38,123 @@ module.exports = app.listen(port, (err) => {
     console.log(err); throw err;
   }
 })
+
+
+//--------------------------------------------------------------------------------------------------
+function registerSimpleRouter() {
+  router.get('/page/get', function (req, res) {
+    const pathname = url.parse(req.url, true).pathname;
+    // res.writeHead(200, {'Content-Type': 'text/plain'}); //下载的
+    // res.setHeader("Content-Type", "text/html");
+    // res.setHeader("Set-Cookie", ["type=ninja", "language=javascript"]);
+    // res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
+    // res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    // res.header("Cache-Control", "no-store");//304
+    // next();
+    res.json({ code: 0, success: true, relust: [{ pathname: pathname }], msg: `你请求的参数成功` })
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(`你的请求路径是${pathname}`);
+    res.end();
+  })
+
+}
+
+function registerBaseRouter() {
+  router.get('/base/get', function (req, res) {
+    res.json(req.query)
+  })
+
+  router.post('/base/post', function (req, res) {
+    res.json(req.body)
+  })
+
+  router.post('/base/buffer', function (req, res) {
+    let msg = []
+    req.on('data', (chunk) => {
+      if (chunk) {
+        msg.push(chunk)
+      }
+    })
+    req.on('end', () => {
+      let buf = Buffer.concat(msg)
+      res.json(buf.toJSON())
+    })
+  })
+}
+
+function registerErrorRouter() {
+  router.get('/error/get', function (req, res) {
+    if (Math.random() > 0.5) {
+      res.json({
+        msg: `hello world`
+      })
+    } else {
+      res.status(500)
+      res.end()
+    }
+  })
+
+  router.get('/error/timeout', function (req, res) {
+    setTimeout(() => {
+      res.json({
+        msg: `hello world`
+      })
+    }, 3000)
+  })
+}
+
+function registerExtendRouter() {
+  router.get('/extend/get', function (req, res) {
+    res.json({
+      msg: 'hello world'
+    })
+  })
+
+  router.options('/extend/options', function (req, res) {
+    res.end()
+  })
+
+  router.delete('/extend/delete', function (req, res) {
+    res.end()
+  })
+
+  router.head('/extend/head', function (req, res) {
+    res.end()
+  })
+
+  router.post('/extend/post', function (req, res) {
+    res.json(req.body)
+  })
+
+  router.put('/extend/put', function (req, res) {
+    res.json(req.body)
+  })
+
+  router.patch('/extend/patch', function (req, res) {
+    res.json(req.body)
+  })
+
+  router.get('/extend/user', function (req, res) {
+    res.json({
+      code: 0,
+      message: 'ok',
+      result: {
+        name: 'jack',
+        age: 18
+      }
+    })
+  })
+}
+
+function registerInterceptorRouter() {
+  router.get('/interceptor/get', function (req, res) {
+    res.end('hello')
+  })
+}
+
+function registerConfigRouter() {
+  router.post('/config/post', function (req, res) {
+    res.json(req.body)
+  })
+}
