a4b4153cd4bb855172acc7027325744f3c5db66f
Kim <7533524@qq.com>
2020-02-16 22:31:42 +0800

    [build] 1.3.5

--- release.sh
+++ release.sh
@@ -10,7 +10,9 @@ funWithReturn(){
     return $(($aNum+$anotherNum))
 }
 # funWithReturn
-echo "请输入的版本号<Enter release version>"
+echo "你当前的版本号为："
+npm view kim-axios version
+echo "请输入的版本号<Enter release version>:"
 read VERSION  #表示从标准输入读取值，并赋值给 $VERSION 变量
 read -p "你确定你的版本号为$VERSION?<Releasing are you sure is $VERSION?> (y/n)" -n 1 -r
 echo  #输出空值表示跳到一个新行，# 在 shell 脚本中表示注释
@@ -22,7 +24,7 @@ then
 
   git add -A  #表示把代码所有变化提交到暂存区
   git commit -m "[build] $VERSION"  #表示提交代码，提交注释是 [build] $VERSION
-  # npm version $VERSION --message "[release] $VERSION" #是修改 package.json 中的 version 字段到 $VERSION，并且提交一条修改记录，提交注释是 [release] $VERSION
+  npm version $VERSION --message "[release] $VERSION" #是修改 package.json 中的 version 字段到 $VERSION，并且提交一条修改记录，提交注释是 [release] $VERSION
   # git remote add origin https://gitee.com/KimGuBa/kim-axios.git
   git push   #是把代码发布到主干分支
   # git push -u origin master  #是把代码发布到主干分支
