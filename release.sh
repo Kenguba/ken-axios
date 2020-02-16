744adf80de09eeab9540de2ecb6c570592792d7b
Kim <7533524@qq.com>
2020-02-16 20:35:00 +0800

    [build] .1.1

--- release.sh
+++ release.sh
@@ -8,12 +8,12 @@ echo  #输出空值表示跳到一个新行，# 在 shell 脚本中表示注释
 #表示 shell 脚本中的流程控制语句，判断 $REPLY 是不是大小写的 y，如果满足，则走到后面的 then 逻辑
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  echo "Releasing $VERSION ..."
+  echo "你的package包的版本已经设置为：$VERSION ..."
 
   git add -A  #表示把代码所有变化提交到暂存区
   git commit -m "[build] $VERSION"  #表示提交代码，提交注释是 [build] $VERSION
-  npm version $VERSION --message "[release] $VERSION"
+  npm version $VERSION --message "[release] $VERSION" #是修改 package.json 中的 version 字段到 $VERSION，并且提交一条修改记录，提交注释是 [release] $VERSION
   # git remote add origin https://gitee.com/KimGuBa/kim-axios.git
-  # git push -u origin master #是修改 package.json 中的 version 字段到 $VERSION，并且提交一条修改记录，提交注释是 [release] $VERSION
+  git push -u origin master  #是把代码发布到主干分支
   npm publish #是把仓库发布到 npm 上，我们会把 dist 目录下的代码都发布到 npm 上，因为我们在 package.json 中配置的是 files 是 ["dist"]
 fi
