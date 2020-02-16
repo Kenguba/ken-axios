5f6f29cac461c0fccf0122131a8f565fc0d8fca6
Kim <7533524@qq.com>
2020-02-16 22:46:26 +0800

    [build] 1.4.5

--- release.sh
+++ release.sh
@@ -29,6 +29,7 @@ then
 
   npm publish #是把仓库发布到 npm 上，我们会把 dist 目录下的代码都发布到 npm 上，因为我们在 package.json 中配置的是 files 是 ["dist"]
   cd ./dist
+  ls .
   git init 
   git add . 
   git commit -m "[build] $VERSION"  #表示提交代码，提交注释是 [build] $VERSION
